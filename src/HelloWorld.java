81a881d3c959a7158d4b4546fbf7a99a49d97f47
vp-pung <pokeang.ung@vecteurplus.com>
2018-07-25 14:47:29 +0700

    ädd one line

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld { 
 	public static void main (String [] args){ 
 		System.out.println("hello world for prod");
+		System.out.println("Add more update for testing deploy using docker");
 	}
 }
