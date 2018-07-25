ac93c7dbbca8c90167c46a9b5992d53af95854b8
vp-pung <pokeang.ung@vecteurplus.com>
2018-07-25 15:34:07 +0700

    update for removed all texts

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,5 @@
 public class HelloWorld { 
 	public static void main (String [] args){ 
-		System.out.println("hello world for prod");
-		System.out.println("Add more update for testing deploy using docker");
+		System.out.println("test update for create new image");
 	}
 }
